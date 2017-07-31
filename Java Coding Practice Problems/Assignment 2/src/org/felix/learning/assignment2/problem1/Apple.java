c8953c4f4ed62d408e435f259bfeae8a7dde48db
Felix Pan <felixpan238@gmail.com>
2017-07-31 13:17:41 -0400

    Completed initial attempt for Assignment 2 Problem 1

---
+++ Java Coding Practice Problems/Assignment 2/src/org/felix/learning/assignment2/problem1/Apple.java
@@ -0,0 +1,49 @@
+package org.felix.learning.assignment2.problem1;
+
+/**
+ * Created by Felix on 2017-07-31.
+ */
+public class Apple {
+    //- Variable declarations and setters and getters ------------------------------------------------------------------
+    private String color;
+    private int weight;
+    private String variety;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    //- Apple constructor ----------------------------------------------------------------------------------------------
+    public Apple(String color, int weight, String variety){
+        this.setColor(color);
+        this.setWeight(weight);
+        this.setVariety(variety);
+    }
+
+    //- Function to print apple's properties ---------------------------------------------------------------------------
+    public void printAppleProperties(){
+        System.out.println(
+                "Color = " + this.getColor() + ", weight = " + this.getWeight() + ", variety = " + this.getVariety());
+    }
+}
+
